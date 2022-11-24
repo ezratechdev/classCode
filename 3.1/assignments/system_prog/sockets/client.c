72aaba7104c308df7d96a332b6462d6b9b393235
ezratechdev <254gtgt@gmail.com>
2022-11-24 23:12:32 +0300

    Update to sockets with c : worked

--- 3.1/assignments/system_prog/sockets/client.c
+++ 3.1/assignments/system_prog/sockets/client.c
@@ -1,42 +1,47 @@
+#include <string.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-void main(int argc, char* argv[])
+int main(int argc, char const* argv[])
 {
-  int sock = 0;
-  int port = 0;
-  struct sockaddr_in servaddr;
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  int status = 0;
-  char buffer[256] = "";
-  if (sock == -1)
-  {
-    printf("could not establish connection\n");
-    exit(1);
-  }
+  const int port = 3400;
+  const char *host_service = "127.0.0.1";
+	int socket_sock_cn = 0, value_read, client_instance;
+	struct sockaddr_in serv_addr;
+	char* hello = "This is the message from the client";
+	char buffer[1024] = { 0 };
+	if ((socket_sock_cn = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Could not create the socket connection\n");
+		return -1;
+	}
 
-  port = 3500;
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = inet_addr(argv[1]);
-  servaddr.sin_port = htons(port);
-  status = connect(sock, (struct sockaddr*) &servaddr, sizeof(servaddr));
-  if (status == 0)
-    printf("connection is established successfully\n");
-  else
-  {
-    printf("could not run the app\n");
-    exit(1);
-  }
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
 
-  status = read(sock, buffer, sizeof(buffer));
-  if (status > 0)
-    printf("%d: %s", status, buffer);
+	// Convert IPv4 and IPv6 addresses from text to binary
+	if (inet_pton(AF_INET, host_service, &serv_addr.sin_addr)
+		<= 0) {
+		printf(
+			"\n The address format passed is not supported \n");
+		return -1;
+	}
 
-  close(sock);
+	if ((client_instance
+		= connect(socket_sock_cn, (struct sockaddr*)&serv_addr,
+				sizeof(serv_addr)))
+		< 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	send(socket_sock_cn , hello, strlen(hello), 0);
+	printf("Message from the client has been sent to the server\n");
+	value_read = read(socket_sock_cn, buffer, 1024);
+	printf("%s\n", buffer);
 
+	// closing the connected socket
+	close(client_instance);
+   // return an integer value as the function is of type integer
+  return 5;
 }
--- 3.1/assignments/system_prog/sockets/server.c
+++ 3.1/assignments/system_prog/sockets/server.c
@@ -1,59 +1,68 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
+#include <stdlib.h>
 #include <string.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-const char message[] = "hello, world\n";
 
-void main()
+int main(int argc, char const *argv[])
 {
-  int sock = 0;
-  int port = 0;
-  sock = socket(AF_INET, SOCK_STREAM, 0);
+  const int port = 3400;
+  int server_intance, new_socket, value_read;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  char *hello = "Hello from the server :)";
 
-  if (sock == -1)
-    fprintf(stderr, "failed\n");
-  else
-    printf("connection is establisshed\n");
-
-  struct sockaddr_in server;
-  server.sin_family = AF_INET;
-  server.sin_addr.s_addr = htonl(INADDR_ANY );
-  server.sin_port = htons(3500);
-
-  int status = bind(sock, (struct sockaddr*) &server, sizeof(server));
-  if (status == 0)
-    printf("connection completed\n");
-  else
-    printf("problem is encountered\n");
-
-  status = listen(sock, 5);
-  if (status == 0)
-    printf("app is ready to work\n");
-  else
+  // Creating socket file
+  if ((server_intance = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
-    printf("connection is failed\n");
-    return 0;
+    perror("socket failed");
+    exit(EXIT_FAILURE);
   }
 
-  while (1)
+  // Forcefully attaching socket to the port 3400
+  if (setsockopt(server_intance, SOL_SOCKET,
+                 SO_REUSEADDR | SO_REUSEPORT, &opt,
+                 sizeof(opt)))
   {
-    struct sockaddr_in client = { 0 };
-    int sclient = 0;
-    // int len = sizeof(client);
-    socklen_t len = sizeof client;
-    int childSocket = accept(sock, (struct sockaddr*) &client, &len);
-    if (childSocket == -1)
-    {
-      printf("cannot accept connection\n");
-      close(sock);
-      break;
-    }
-
-    write(childSocket, message, strlen(message));
-
-    close(childSocket);
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
   }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(port);
 
+  // Forcefully attaching socket to the port 3400
+  if (bind(server_intance, (struct sockaddr *)&address,
+           sizeof(address)) < 0)
+  {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+  if (listen(server_intance, 3) < 0)
+  {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+  if ((new_socket = accept(server_intance, (struct sockaddr *)&address,
+                           (socklen_t *)&addrlen)) < 0)
+  {
+    perror("accept");
+    exit(EXIT_FAILURE);
+  }
+  value_read = read(new_socket, buffer, 1024);
+  printf("%s\n", buffer);
+  send(new_socket, hello, strlen(hello), 0);
+  printf("Hello message sent\n");
+
+  // closing the connected socket
+  close(new_socket);
+  // closing the listening socket
+  shutdown(server_intance, SHUT_RDWR);
+
+  // return an integer value as the function is of type integer
+  return 5;
 }

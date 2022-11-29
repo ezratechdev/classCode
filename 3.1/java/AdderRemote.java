dbf979f5911460692d179a1bd39953dcd01c2f74
ezratechdev <254gtgt@gmail.com>
2022-11-29 13:31:23 +0300

    Changes?

--- 3.1/java/AdderRemote.java
+++ 3.1/java/AdderRemote.java
@@ -8,6 +8,6 @@ super();
 }
 
 // public int add(int x,int y){return x+y;}
-public double area(double radius){return ( radius * radius * 3.14);}
+public double area(double radius){return ( radius * radius * (22 / 7));}
 
 }
